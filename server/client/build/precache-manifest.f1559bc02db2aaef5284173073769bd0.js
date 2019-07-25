098b65c2d6e9f190f043fa2abd4a4011081eae93
amerzbach <adi.merzbach@gmail.com>
2019-07-25 17:26:22 +0200

    done

---
+++ server/client/build/precache-manifest.f1559bc02db2aaef5284173073769bd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710631a1f2410b5c56835cb3180871b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8ce38de0e29fa2d0e9c6",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "1bc4bf89d61a4fb8dfe7",
+    "url": "/static/css/main.86fbbe60.chunk.css"
+  },
+  {
+    "revision": "8ce38de0e29fa2d0e9c6",
+    "url": "/static/js/2.cc92e594.chunk.js"
+  },
+  {
+    "revision": "1bc4bf89d61a4fb8dfe7",
+    "url": "/static/js/main.ab473f3d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
